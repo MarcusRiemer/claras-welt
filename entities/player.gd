cb2140b432ed46065f2e436e2049c38937ffc54c
Marcus Riemer <post@marcusriemer.de>
2024-12-02 17:39:35 +0100

    Wolke & Tileset

--- entities/player.gd
+++ entities/player.gd
@@ -3,8 +3,11 @@ extends CharacterBody2D
 @export var speed = 400
 
 func get_input():
-	var input_direction = Input.get_vector("left", "right", "up", "down")
-	velocity = input_direction * speed
+	if Input.is_action_just_pressed("reset"):
+		position = Vector2(0, 0)
+	else:
+		var input_direction = Input.get_vector("left", "right", "up", "down")
+		velocity = input_direction * speed
 
 func _physics_process(delta):
 	get_input()
